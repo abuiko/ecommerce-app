a3a33af0b0cdfc5795ec9da62b1cb0f9b80e8df0
Alena Buiko <abuiko.ma@gmail.com>
2022-01-25 00:03:40 -0500

    add slider

---
+++ src/data.js
@@ -0,0 +1,24 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://images.unsplash.com/photo-1551488831-00ddcb6c6bd3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+        title: "WINTER SALE",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: "https://images.unsplash.com/photo-1560243563-062bfc001d68?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+        title: "NEW ARRIVALS",
+        desc: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.",
+        bg: "fcf1ed",
+    },
+    {
+        id: 3,
+        img: "https://images.unsplash.com/photo-1490114538077-0a7f8cb49891?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+        title: "COSY BASICS",
+        desc: "Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?",
+        bg: "fbf0f4",
+    },
+
+]
