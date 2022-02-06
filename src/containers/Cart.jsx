92c590cb697dcbf8b830b7012b34ac6e4ae58c6b
Alena Buiko <abuiko.ma@gmail.com>
2022-02-05 20:43:15 -0500

    add empty cart ui

--- src/containers/Cart.jsx
+++ src/containers/Cart.jsx
@@ -5,9 +5,10 @@ import Footer from '../components/Footer'
 import styled from 'styled-components'
 
 import CartItem from '../components/CartItem'
-import { Context } from '../Context'
+// import { Context } from '../Context'
 
 import { device } from '../responsive'
+import { ShoppingCartOutlined } from '@material-ui/icons'
 
 const Container = styled.div``
 
@@ -108,31 +109,94 @@ const Button = styled.button`
     font-weight: 600;
     border: none;
 `
+const EmptyCartWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    min-height: 60vh;
+    background-color: #F8F8F8;
+   
+`
 
+const Icon = styled.div`
+    border: 1px solid #000;
+    width: 60px;
+    height: 60px;
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 20px;
+    background-color: white;
+`
+const EmptyCartText = styled.p`
+    font-size: 22px;
+    font-weight: 300;
+    margin-bottom: 20px;
+`
+const ButtonContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    
+`
+const ButtonMenu = styled.button`
+    width: 200px;
+    padding: 10px 20px;
+    border: 1px solid #000;
+    background: white;
+    color: #000;
+    font-weight: 500;
+    font-size: 16px;
+    margin-right: ${props => props.type === "leftBtn" && "10px"};
+    cursor: pointer;
+    transition: all 0.3s ease-in-out;
+    &:hover {
+        background-color: #000;
+        color: white;
+    }
+
+    @media screen and ${device.mobile} {
+        margin-right: 0px;
+        margin-bottom: 10px;
+      
+    }
+    
+`
 const Cart = () => {
-    const { cartItems } = useContext(Context)
-    const itemsInCart = cartItems.map(item => <CartItem item={item} key={item.id} />)
+    // const { cartItems } = useContext(Context)
+    // const itemsInCart = cartItems.map(item => <CartItem item={item} key={item.id} />)
 
 
     return (
         <Container>
             <Announcement />
             <Navbar />
-            <Wrapper>
+            <EmptyCartWrapper>
+                <Icon>
+                    <ShoppingCartOutlined style={{ fontSize: "35px" }} />
+                </Icon>
+                <EmptyCartText>Your Shopping Bag is Empty</EmptyCartText>
+                <ButtonContainer>
+                    <ButtonMenu type="leftBtn">Shop Women's</ButtonMenu>
+                    <ButtonMenu>Shop Men's</ButtonMenu>
+                </ButtonContainer>
+
+            </EmptyCartWrapper>
+            {/* <Wrapper>
                 <Title>YOUR CART</Title>
                 <Top>
                     <TopButton>CONTINUE SHOPPING</TopButton>
                     <TopTexts>
-                        <TopText>Items In Cart ({itemsInCart.length})</TopText>
+                        
+                        <TopText>Items In Cart (0)</TopText>
 
                     </TopTexts>
                     <TopButton type="filled">CHECKOUT NOW</TopButton>
                 </Top>
                 <Bottom>
-                    <Info>
-                        {itemsInCart}
-
-                    </Info>
+                    <Info></Info>
                     <Summary>
                         <SummaryTitle>ORDER SUMMARY</SummaryTitle>
                         <SummaryItem>
@@ -154,7 +218,7 @@ const Cart = () => {
                         <Button>CHECKOUT NOW</Button>
                     </Summary>
                 </Bottom>
-            </Wrapper>
+            </Wrapper> */}
             <Footer />
         </Container>
     )
